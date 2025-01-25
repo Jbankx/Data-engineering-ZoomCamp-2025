b62d7e891e699f030d9379edecec6e29c57c3a95
Jbankx <junioralhassan92@gmail.com>
2025-01-25 18:58:04 +0100

    week 1 homework additions

--- 01-docker-terraform/2_docker_sql/pipeline.py
+++ 01-docker-terraform/2_docker_sql/pipeline.py
@@ -9,3 +9,8 @@ day = sys.argv[1]
 # some fancy stuff with pandas
 
 print(f'job finished successfully for day = {day}')
+
+# Note: you can also dockerise a data pipeline (Watch DE Zoomcamp 1.2.4) 
+# https://www.youtube.com/watch?v=B1WwATwf-vY&list=PL3MmuxUbc_hJed7dXYoJw8DoCuVHhGEQb&index=8
+
+# Project focuses on using "Airflow" instead of the above (out of project scope)
