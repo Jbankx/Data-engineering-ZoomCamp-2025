1fb2c0a8faf70e66a446936076e9c43fd9656a16
Jbankx <junioralhassan92@gmail.com>
2025-03-24 11:46:48 +0100

    python tests

---
+++ my-notes/python_test.py
@@ -0,0 +1,15 @@
+# Prompt the user to insert the first number
+first_number = input("Please insert first number: ")
+
+# Prompt the user to insert the second number
+second_number = input("Please insert second number: ")
+
+# Convert input to integers
+first_number = int(first_number)
+second_number = int(second_number)
+
+# Perform addition
+result = first_number + second_number
+
+# Print the result
+print(f"The sum of {first_number} and {second_number} is: {result}")
