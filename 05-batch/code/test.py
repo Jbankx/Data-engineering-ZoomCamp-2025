555b1e1ee65d985fbb085fe5f8e33eb1214d1053
Jbankx <junioralhassan92@gmail.com>
2025-04-03 13:52:27 +0200

    added test.py

---
+++ 05-batch/code/test.py
@@ -0,0 +1,16 @@
+#To run it use >>>  python test.py
+
+
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .master("local[*]") \
+    .appName('test') \
+    .getOrCreate()
+
+df = spark.read \
+    .option("header", "true") \
+    .csv('taxi_zone_lookup.csv')
+
+df.show()
